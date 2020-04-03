cfc363e036bfca46720686917f53cf8a8bf715a1
varun-vp <varun.pahuja96@gmail.com>
2020-04-03 12:24:36 +0530

    first

---
+++ Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitBranchConceptExmaple.Models
+{
+    public class Person
+    {
+        [Key]
+        public int PId { get; set; }
+        public string PName{ get; set; }
+        public int Page { get; set; }
+    }
+}
